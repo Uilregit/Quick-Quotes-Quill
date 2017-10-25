840d6b178f6e68cf461839737ea949f0ecd0e0fd
Tiger Liu <uilregit@gmail.com>
2017-10-24 22:56:05 -0400

    Added Transcription() funtion
    Transcription () is now used instead of Listen(). 
    Method names have been updated to follow the pascale capitalization.
    
    Transcription() tries to process the spoken audio as well as logs it in the Transcription.txt file. Each transcription is logged on a different line seperated by "----------".

--- QuickQuotesQuill.py
+++ QuickQuotesQuill.py
@@ -2,27 +2,47 @@ import speech_recognition as sr
 
 r = sr.Recognizer()
 #r.recognize_google (audio, key = "94f421a6c116086bdcddbdd5accdd4c072cfb164")
-def listen ():
-    with sr.Microphone() as source:
-        print ("Say something!")
-        audio = r.listen(source)
 
+def Transcribe():
+    r = open ("Transcriptions.txt", "r")
+    readTexts = r.read()
+    d = open ("Transcriptions.txt", "w")
+    d.write(readTexts)
+    d.write (Listen()+"\n")
+    #d.write ("stuff"+"\n")
+    d.write ("----------\n")
+    d.close()
+    r.close()
+
+def Listen ():
+    sentence = SpeechRecognizer()
+    if type(sentence) == str:
+        FindSubStringLocation(sentence)
+        return sentence
+    else:
+        return "Error: Google Speech Recognition could not understand audio"
+    
+    
+def SpeechRecognizer():
+    with sr.Microphone() as source:
+        print ("Recording...")
+        audio = r.listen(source)
     try:
         sentence = r.recognize_google(audio)
         print ("'"+sentence+"'")
-        findSubStringLocation (sentence)
+        return sentence
     except sr.UnknownValueError:
         print("Google Speech Recognition could not understand audio")
     except sr.RequestError as e:
         print("Could not request results from Google Speech Recognition service; {0}".format(e))
 
-def findSubStringLocation (text):
+def FindSubStringLocation (text):
     triggerPhraseList = "I'm going to give you "
     index = text.find(triggerPhraseList)
     if index == -1:
         print("Could not find prescription")
     else:
-        findPrescription (text, index+len(triggerPhraseList))
+        FindPrescription (text, index+len(triggerPhraseList))
 
-def findPrescription (text, index):
-    print( text[index:])
+def FindPrescription (text, index):
+    print(text[index:])
